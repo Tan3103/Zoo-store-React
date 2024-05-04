83e9a324fc49f4fd7154a03ba65ada69a62279ab
Tannur <tannur.best@gmail.com>
2024-05-04 22:25:34 +0600

    New

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Footer() {
-  return <footer>&copy; 2022 - Uatayev Tannur, Slam Olzhas</footer>;
+  return <footer>&copy; 2022 - Uatayev Tannur</footer>;
 }
